11eec4dc8778c0f33760dc62bad402707de75136
akermanai <akerman@rigstone.com>
2026-04-29 17:12:34 +0200

    revert: restore next.config.js to last working state (ddf54915)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true }
 }
 module.exports = nextConfig
